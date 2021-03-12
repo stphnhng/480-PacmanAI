ebf0c61a6bd33a15ba13e73efbd13c1594724b26
Wesly Ortega <weslyortega@gmail.com>
2021-03-11 23:59:22 -0800

    forgot to change something xd

--- pacman.py
+++ pacman.py
@@ -776,7 +776,7 @@ def readCommand(argv):
     parser.add_option('-l', '--layout', dest='layout',
                       help=default(
                           'the LAYOUT_FILE from which to load the map layout'),
-                      metavar='LAYOUT_FILE', default='trickyClassic')
+                      metavar='LAYOUT_FILE', default='smallClassic')
     parser.add_option('-p', '--pacman', dest='pacman',
                       help=default(
                           'the agent TYPE in the pacmanAgents module to use'),
